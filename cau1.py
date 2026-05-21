33fa7c6799cef275af8e072b58c558f5971f85b8
QuangKiet <quangkiet1508@gmail.com>
2026-05-21 21:08:35 +0700

    Update cau1.py

--- cau1.py
+++ cau1.py
@@ -1,1 +1,10 @@
-
+n = int(input("Nhap so co 4 chu so bat ki: "))
+tong = 0
+tong = tong + n % 10
+n = n // 10
+tong = tong + n % 10
+n = n // 10
+tong = tong + n % 10
+n = n // 10
+tong = tong + n
+print("tong cua 4 chu so do la: ",tong)
