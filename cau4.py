ec22d0a1f33c68115c5ebdeec601615a8ebb2450
QuangKiet <quangkiet1508@gmail.com>
2026-05-21 21:47:31 +0700

    Update cau4.py

--- cau4.py
+++ cau4.py
@@ -1,1 +1,9 @@
-
+can_nang = float(input("Nhap can nang cua ban (kg): "))
+chieu_cao = float(input("Nhap chieu cao cua ban (m): "))
+BMI = round(can_nang / (chieu_cao ** 2),2)
+if BMI < 18.5:
+    print("Ban bi gay")
+elif BMI < 25:
+    print("Ban binh thuong")
+else:
+    print("Ban hoi thua can")
